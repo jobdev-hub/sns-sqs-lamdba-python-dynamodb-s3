11479e853f5b250cb8f2ee6d8f12e9bebee76505
jobdev-hub <job.methods@outlook.com>
2022-05-13 11:22:12 -0300

    example test aws

---
+++ lambda_function_dynamodb_example.py
@@ -0,0 +1,24 @@
+import json
+import boto3
+import uuid
+from decimal import Decimal
+
+def lambda_handler(event, context):
+       
+    body = json.loads(event["Records"][0]["body"])
+    message = json.loads(body["Message"], parse_float = Decimal)
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("table-test")
+    
+    table.put_item(
+        Item = {
+            "id" : str(uuid.uuid4()),
+            "content" : message
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Success! Put Item DynamoDB')
+    }
---
+++ lambda_function_s3_example.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+    s3 = boto3.client("s3")
+    
+    body = json.loads(event["Records"][0]["body"])
+    message = body ["Message"]
+    
+    s3.put_object(
+        Body=message,
+        Bucket="bucket-test",
+        Key=f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.txt"
+    )
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Success! Put Object in S3')
+    }
